57d8ded1fb32e3f226391ab9f4224e569132672a
nojmana <neumannkinga7@gmail.com>
2020-03-24 23:27:17 +0100

    Add get list of comments from a specific pull request

---
+++ GitHubAPI/github_api.py
@@ -0,0 +1,21 @@
+import json
+import urllib.request
+
+
+def get_pull_request_comments(user, project):
+    base_url = 'https://api.github.com/repos/'
+    project_url = base_url + user + '/' + project + '/pulls/49/comments'
+    print(project_url)
+    with urllib.request.urlopen(project_url) as url:
+        json_data = json.loads(url.read().decode())
+        comments = []
+        for comment_data in json_data:
+            comments.append(comment_data['body'])
+        return comments
+
+
+if __name__ == "__main__":
+    user = 'eclipse'
+    project = 'openj9'
+    data = get_pull_request_comments(user, project)
+    print(data)

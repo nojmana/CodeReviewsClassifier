f893c37b0a043cc5fe240e0ff823f5b6b42973e8
nojmana <neumannkinga7@gmail.com>
2020-03-29 23:52:25 +0200

    Add authorization to increase requests limit, add author column to the result

--- GitHubAPI/github_api.py
+++ GitHubAPI/github_api.py
@@ -4,20 +4,42 @@ import urllib.request
 
 class GitHubApi:
 
-    def __init__(self, user, project, number_of_results):
+    def __init__(self, user, project, number_of_results, auth_token):
         self.user = user
         self.project = project
         self.number_of_results = number_of_results
+        self.auth_token = auth_token
+
+    def authorization_header(self):
+        headers = {'Authorization': 'token %s' % self.auth_token}
+        return headers
+
+    def get_pull_request_author(self, comment):
+        request = urllib.request.Request(comment['pull_request_url'], headers=self.authorization_header())
+        with urllib.request.urlopen(request) as requested_url:
+            json_data = json.loads(requested_url.read().decode())
+            return json_data['user']['id']
+
+    def check_if_reviewer_is_author(self, comment):
+        comment_author = comment['user']['id']
+        if comment_author == self.get_pull_request_author(comment):
+            return True
+        else:
+            return False
 
     def get_pull_request_comments_one_page(self, page_number):
         url = 'https://api.github.com/repos/' + self.user + '/' + self.project + \
               '/pulls/comments?per_page=100&page=' + str(page_number)
-        with urllib.request.urlopen(url) as requested_url:
+        request = urllib.request.Request(url, headers=self.authorization_header())
+        with urllib.request.urlopen(request) as requested_url:
             json_data = json.loads(requested_url.read().decode())
             page_comments = list()
-            for comment_data in json_data:
-                page_comments.append({"FILENAME": comment_data["path"],
-                                      "BODY": comment_data["body"]})
+            for comment in json_data:
+                page_comments.append({'FILENAME': comment['path'],
+                                      'CR AUTHOR = PR AUTHOR': self.check_if_reviewer_is_author(comment),
+                                      'BODY': comment['body']})
+                if len(page_comments) >= self.number_of_results:
+                    break
             return page_comments
 
     def get_pull_request_comments_all_pages(self):
@@ -25,7 +47,7 @@ class GitHubApi:
         page_number = 0
         number_of_results_counter = 0
         while number_of_results_counter < self.number_of_results:
-            page_comments = self.get_pull_request_comments_one_page(page_number)[:self.number_of_results]
+            page_comments = self.get_pull_request_comments_one_page(page_number)
             if len(page_comments) == 0:
                 break
             number_of_results_counter += len(page_comments)
--- GitHubAPI/main_script.py
+++ GitHubAPI/main_script.py
@@ -10,11 +10,18 @@ def write_to_csv(data, user, project):
     df.to_csv(filename, index=False, header=True)
 
 
+def read_token():
+    file = open('token.txt', 'r')
+    return file.read().split()[0]
+
+
 if __name__ == "__main__":
     user = sys.argv[1]
     project = sys.argv[2]
     number_of_results = int(sys.argv[3])
 
-    github_api = GitHubApi(user, project, number_of_results)
+    auth_token = read_token()
+
+    github_api = GitHubApi(user, project, number_of_results, auth_token)
     comments = github_api.get_pull_request_comments_all_pages()
     write_to_csv(comments, user, project)

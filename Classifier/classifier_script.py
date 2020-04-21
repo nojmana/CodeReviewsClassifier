8c1a1356c258b8d6855f6512938046f31d06a546
nojmana <neumannkinga7@gmail.com>
2020-04-21 22:21:16 +0200

    Refactor of logistic_regression, add tuned parameters

--- Classifier/classifier_script.py
+++ Classifier/classifier_script.py
@@ -1,5 +1,5 @@
-from Classifier import input_reader, bag_of_words, logistic_regression
-from Classifier.grid_display import GridSearch_table_plot
+
+from Classifier import input_reader, bag_of_words, classifiers
 
 gerrit_file = 'gerrit-wireshark-train-test-v4.xlsx'
 seed = 40
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     data_set_x = list([row['message'] for row in data_set])
     data_set_y = list([row['purpose'] for row in data_set])
-    data_set_y = input_reader.encode_labels(data_set_y)
+    # data_set_y = input_reader.encode_labels(data_set_y)
 
     train_set_x, test_set_x = input_reader.split_set(data_set_x, 0.9, seed)
     train_set_y, test_set_y = input_reader.split_set(data_set_y, 0.9, seed)
@@ -19,10 +19,5 @@ if __name__ == "__main__":
     train_bow_model = bag_of_words.create_bow_model(train_set_x, frequent_words_count)
     test_bow_model = bag_of_words.create_bow_model(test_set_x, frequent_words_count)
 
-    log_reg = logistic_regression.LogisticRegressionImpl(seed)
-    grid_search = log_reg.grid_search()
-    grid_search.fit(train_bow_model, train_set_y)
-    predictions = grid_search.predict(test_bow_model)
-    log_reg.measure_efficiency(test_set_y, predictions)
-
-    GridSearch_table_plot(grid_search, "C", graph=False, negative=False)
+    classifier = classifiers.Classifier(seed, train_bow_model, train_set_y, test_bow_model, test_set_y)
+    classifier.logistic_regression()
---
+++ Classifier/classifiers.py
@@ -0,0 +1,33 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
+from Classifier.grid_display import GridSearch_table_plot
+
+
+def measure_efficiency(y, predictions):
+    print('Accuracy Score : ', str(accuracy_score(y, predictions)))
+
+
+class Classifier:
+    def __init__(self, seed, train_bow_model, train_set_y, test_bow_model, test_set_y):
+        self.seed = seed
+        self.train_bow_model = train_bow_model
+        self.train_set_y = train_set_y
+        self.test_bow_model = test_bow_model
+        self.test_set_y = test_set_y
+
+    def logistic_regression(self):
+        log_reg = LogisticRegression(random_state=self.seed, max_iter=1000000, solver='liblinear', C=0.1)
+        log_reg.fit(self.train_bow_model, self.train_set_y)
+        predictions = log_reg.predict(self.test_bow_model)
+        measure_efficiency(self.test_set_y, predictions)
+
+    def grid_search_logistic_regression(self):
+        param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
+                      'solver': ['liblinear', 'newton-cg', 'sag', 'saga', 'lbfgs']}
+        grid_search = GridSearchCV(LogisticRegression(random_state=self.seed, max_iter=1000000), param_grid, verbose=1)
+        grid_search.fit(self.train_bow_model, self.train_set_y)
+        predictions = grid_search.predict(self.test_bow_model)
+        measure_efficiency(self.test_set_y, predictions)
+        GridSearch_table_plot(grid_search, "C", graph=False, negative=False)

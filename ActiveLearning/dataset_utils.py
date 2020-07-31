a845e8b4a67c5022e3650cfb0617d43706068d3d
nojmana <neumannkinga7@gmail.com>
2020-07-31 19:24:48 +0200

    Update stopwords

--- ActiveLearning/dataset_utils.py
+++ ActiveLearning/dataset_utils.py
@@ -32,11 +32,11 @@ def tokenize(data_set):
     tokenizer = RegexpTokenizer("[a-zA-Z@]+")
     stemmer = SnowballStemmer("english")
     stop = stopwords.words('english')
-    excluding = ['against', 'not', 'don', "don't", 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't",
+    excluding = ['against', 'no', 'not', 'don', "don't", 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't",
                  'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't",
-                 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shouldn', "shouldn't", 'wasn',
-                 "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't",
-                'same', 'here', 'likewise', 'above', 'below']
+                 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'should', 'shouldn', "shouldn't",
+                 "should've" 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't",
+                 'same', 'here', 'likewise', 'above', 'below', 'again', ]
     stop = [word for word in stop if word not in excluding]
 
     tokenized_data = []
